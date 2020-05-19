94f339486abf8d0eb38a30d89679cfda34d29732
FredolaPower <frederic.leclere@insea.edu>
2020-05-19 20:30:14 +0200

    Made with love <3

--- SeekFlee/Helper/DrawVector.gd
+++ SeekFlee/Helper/DrawVector.gd
@@ -3,6 +3,7 @@ extends Node2D
 ### PROPERTIES ###
 
 # EXPORT
+export(NodePath) var PROPERTY_OWNER
 export(String) var PROPERTY # Property holding the vector
 export(Color) var COLOR # Vector color
 export(bool) var AA # Antialiasing
@@ -13,14 +14,14 @@ func _physics_process(delta: float) -> void:
 
 
 func _draw():
-	var rotation = Vector2(1,0).angle_to(get_parent().get(PROPERTY)) # Determine the angle to rotate the triangle
+	var rotation = Vector2(1,0).angle_to(get_node(PROPERTY_OWNER).get(PROPERTY)) # Determine the angle to rotate the triangle
 	
 	var triangle: PoolVector2Array = [
-		Vector2(0,0).rotated(rotation) + get_parent().get(PROPERTY),
-		Vector2(0,-10).rotated(rotation) + get_parent().get(PROPERTY),
-		Vector2(25,0).rotated(rotation) + get_parent().get(PROPERTY),
-		Vector2(0,10).rotated(rotation) + get_parent().get(PROPERTY)
+		Vector2(0,0).rotated(rotation) + get_node(PROPERTY_OWNER).get(PROPERTY),
+		Vector2(0,-10).rotated(rotation) + get_node(PROPERTY_OWNER).get(PROPERTY),
+		Vector2(25,0).rotated(rotation) + get_node(PROPERTY_OWNER).get(PROPERTY),
+		Vector2(0,10).rotated(rotation) + get_node(PROPERTY_OWNER).get(PROPERTY)
 	]
 	
-	draw_line(Vector2.ZERO, get_parent().get(PROPERTY), COLOR, 3, AA) # Draw the line
+	draw_line(Vector2.ZERO, get_node(PROPERTY_OWNER).get(PROPERTY), COLOR, 3, AA) # Draw the line
 	draw_colored_polygon(triangle, COLOR, PoolVector2Array(), null, null, AA) # Draw the triangle
