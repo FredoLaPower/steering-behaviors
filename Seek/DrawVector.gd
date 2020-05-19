19ee38a9d3b06cfda80f3f8cda03897719976fff
FredolaPower <frederic.leclere@insea.edu>
2020-05-19 18:05:53 +0200

    Added comments

--- Seek/DrawVector.gd
+++ Seek/DrawVector.gd
@@ -1,16 +1,19 @@
 extends Node2D
 
+### PROPERTIES ###
+
+# EXPORT
 export(String) var PROPERTY # Property holding the vector
 export(Color) var COLOR # Vector color
 
 
 # warning-ignore:unused_argument
 func _physics_process(delta: float) -> void:
-	update()
+	update() # Called to redraw the arrow
 
 
 func _draw():
-	var rotation = Vector2(1,0).angle_to(get_parent().get(PROPERTY))
+	var rotation = Vector2(1,0).angle_to(get_parent().get(PROPERTY)) # Determine the angle to rotate the triangle
 	
 	var triangle: PoolVector2Array = [
 		Vector2(0,0).rotated(rotation) + get_parent().get(PROPERTY),
@@ -19,5 +22,5 @@ func _draw():
 		Vector2(0,10).rotated(rotation) + get_parent().get(PROPERTY)
 	]
 	
-	draw_line(Vector2.ZERO, get_parent().get(PROPERTY), COLOR, 3, true)
-	draw_colored_polygon(triangle, COLOR,  PoolVector2Array(), null, null, true)
+	draw_line(Vector2.ZERO, get_parent().get(PROPERTY), COLOR, 3, true) # Draw the line
+	draw_colored_polygon(triangle, COLOR, PoolVector2Array(), null, null, true) # Draw the triangle
